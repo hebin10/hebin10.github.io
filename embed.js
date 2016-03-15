bd11eb8bfbf80775f6cc5e563f15333c16559cb3
hebin <491309649@qq.com>
2016-03-15 10:56:50 +0800

    Site updated: 2016-03-15 10:56:50

--- embed.js
+++ embed.js
@@ -1,50 +1,100 @@
-//HelloDog -  http://wsgzao.github.io/post/duoshuo/
-//Mobile Start
-
-function checkMobile() {
-    var isiPad = navigator.userAgent.match(/iPad/i) != null;
-    if (isiPad) {
-        return false;
-    }
-    var isMobile = navigator.userAgent.match(/iphone|android|phone|mobile|wap|netfront|x11|java|opera mobi|opera mini|ucweb|windows ce|symbian|symbianos|series|webos|sony|blackberry|dopod|nokia|samsung|palmsource|xda|pieplus|meizu|midp|cldc|motorola|foma|docomo|up.browser|up.link|blazer|helio|hosin|huawei|novarra|coolpad|webos|techfaith|palmsource|alcatel|amoi|ktouch|nexian|ericsson|philips|sagem|wellcom|bunjalloo|maui|smartphone|iemobile|spice|bird|zte-|longcos|pantech|gionee|portalmmm|jig browser|hiptop|benq|haier|^lct|320x320|240x320|176x220/i) != null;
-    if (isMobile) {
-        return true;
-    }
-    return false;
-}
-//Mobile End
-//Admin Start
-
+//判断是否为博主
 function sskadmin(e) {
     var ssk = '';
-    if (e.user_id == 867394) {
-        if (checkMobile()) {
-            ssk = '<span class="this_ua sskadmin">R00T</span><br><br>';
+    if(e.user_id==【你的多说id】){
+        ssk = '<span class="sskadmin">博主【此处可以自定义文字】'
+    }
+    return ssk+"</span> ";
+}
+//显UA开始
+function ua(e) {
+        var r = new Array;
+        var outputer = '';
+        if (r = e.match(/MSIE\s([^\s|;]+)/gi)) {
+            outputer = '<span class="ua_ie">Internet Explorer' + '|' + r[0]/*.replace('MSIE', '').split('.')[0]*/
+        } else if (r = e.match(/FireFox\/([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_firefox">Mozilla FireFox' + '|' + r1[1]
+        } else if (r = e.match(/Maxthon([\d]*)\/([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_maxthon">Maxthon'
+        } else if (r = e.match(/UBrowser([\d]*)\/([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_ucweb">UCBrowser' + '|' + r1[1]
+        } else if (r = e.match(/MetaSr/ig)) {
+            outputer = '<span class="ua_sogou">搜狗浏览器'
+        } else if (r = e.match(/2345Explorer/ig)) {
+            outputer = '<span class="ua_2345explorer">2345王牌浏览器'
+        } else if (r = e.match(/2345chrome/ig)) {
+            outputer = '<span class="ua_2345chrome">2345加速浏览器'
+        } else if (r = e.match(/LBBROWSER/ig)) {
+            outputer = '<span class="ua_lbbrowser">猎豹安全浏览器'
+        } else if (r = e.match(/MicroMessenger\/([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_qq">微信' + '|' + r1[1]/*.split('/')[0]*/
+        } else if (r = e.match(/QQBrowser\/([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_qq">QQ浏览器' + '|' + r1[1]/*.split('/')[0]*/
+        } else if (r = e.match(/QQ\/([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_qq">QQ浏览器' + '|' + r1[1]/*.split('/')[0]*/
+        } else if (r = e.match(/MiuiBrowser\/([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_mi">Miui浏览器' + '|' + r1[1]/*.split('/')[0]*/
+        } else if (r = e.match(/Chrome([\d]*)\/([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_chrome">Chrome' + '|' + r1[1]/*.split('.')[0]*/
+        } else if (r = e.match(/safari\/([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_apple">Apple Safari' + '|' + r1[1]
+        } else if (r = e.match(/Opera[\s|\/]([^\s]+)/ig)) {
+            var r1 = r[0].split("/");
+            outputer = '<span class="ua_opera">Opera' + '|' + r[1]
+        } else if (r = e.match(/Trident\/7.0/gi)) {
+            outputer = '<span class="ua_ie">Internet Explorer 11'
         } else {
-            ssk = '<span class="this_ua sskadmin">R00T</span>';
+            outputer = '<span class="ua_other">其它浏览器'
         }
-    } else {
-        if (checkMobile()) {
-            ssk = '<br><br>';
+        return outputer+"</span> ";
+    }
+    function os(e) {
+        var os = '';
+        if (e.match(/win/ig)) {
+            if (e.match(/nt 5.1/ig)) {
+                os = '<span class="os_xp">Windows XP'
+            } else if (e.match(/nt 6.1/ig)) {
+                os = '<span class="os_7">Windows 7'
+            } else if (e.match(/nt 6.2/ig)) {
+                os = '<span class="os_8">Windows 8'
+            } else if (e.match(/nt 6.3/ig)) {
+                os = '<span class="os_8_1">Windows 8.1'
+            } else if (e.match(/nt 10.0/ig)) {
+                os = '<span class="os_8_1">Windows 10'
+            } else if (e.match(/nt 6.0/ig)) {
+                os = '<span class="os_vista">Windows Vista'
+            } else if (e.match(/nt 5/ig)) {
+                os = '<span class="os_2000">Windows 2000'
+            } else {
+                os = '<span class="os_windows">Windows'
+            }
+        } else if (e.match(/android/ig)) {
+            os = '<span class="os_android">Android'
+        } else if (e.match(/ubuntu/ig)) {
+            os = '<span class="os_ubuntu">Ubuntu'
+        } else if (e.match(/linux/ig)) {
+            os = '<span class="os_linux">Linux'
+        } else if (e.match(/mac/ig)) {
+            os = '<span class="os_mac">Mac OS X'
+        } else if (e.match(/unix/ig)) {
+            os = '<span class="os_unix">Unix'
+        } else if (e.match(/symbian/ig)) {
+            os = '<span class="os_nokia">Nokia SymbianOS'
+        } else {
+            os = '<span class="os_other">其它操作系统'
         }
+        return os+"</span>" ;
     }
-    return ssk;
-}
-//Admin End
-//UA Start
-
-function show_ua(string) {
-    console.log(string)
-    $.ua.set(string);
-    var sua = $.ua;
-    if (sua.os.version == 'x86_64') sua.os.version = 'x64';
-    if (checkMobile()) {
-        return '<span class="this_ua platform ' + sua.os.name + '">' + sua.os.name + ' ' + sua.os.version + '</span><br><br><span class="this_ua browser ' + sua.browser.name + '">' + sua.browser.name + ' ' + sua.browser.version + '</span>';
-    } else {
-        return '<span class="this_ua platform ' + sua.os.name + '">' + sua.os.name + ' ' + sua.os.version + '</span><span class="this_ua browser ' + sua.browser.name + '">' + sua.browser.name + ' ' + sua.browser.version + '</span>';
-    }
-}
-//UA End
+//显UA结束
 !
 function(e, t, s) {
     function a() {
@@ -721,7 +771,7 @@ function(e, t, s) {
                 s = e.post,
                 i = e.options,
                 r = s.author;
-            if (t += '<li class="ds-post" data-post-id="' + s.post_id + '"><div class="ds-post-self" data-post-id="' + s.post_id + '" data-thread-id="' + s.thread_id + '" data-root-id="' + s.root_id + '" data-source="' + s.source + '"><div class="ds-avatar"', r.user_id && (t += ' data-user-id="' + r.user_id + '"'), t += ">" + et.avatar(r), S.sourceName[s.source] && (t += et.serviceIcon(s.source)), t += '</div><div class="ds-comment-body"><div class="ds-comment-header">', r.url ? (t += '<a class="ds-user-name ds-highlight" data-qqt-account="' + (r.qqt_account || "") + '" href="' + u(r.url) + '" ', r.user_id && (t += " onclick=\"this.href='" + a() + "/user-url/?user_id=" + r.user_id + "';\""), t += ' rel="nofollow" target="_blank"', r.user_id && (t += ' data-user-id="' + r.user_id + '"'), t += ">" + u(r.name) + show_ua(e.agent) + "</a>") : (t += '<span class="ds-user-name"', r.user_id && (t += ' data-user-id="' + r.user_id + '"'), t += ' data-qqt-account="' + (r.qqt_account || "") + '">' + u(r.name) + "</span>"), t += "</div>", 1 == i.max_depth && i.show_context && s.parents.length) {
+            if (t += '<li class="ds-post" data-post-id="' + s.post_id + '"><div class="ds-post-self" data-post-id="' + s.post_id + '" data-thread-id="' + s.thread_id + '" data-root-id="' + s.root_id + '" data-source="' + s.source + '"><div class="ds-avatar"', r.user_id && (t += ' data-user-id="' + r.user_id + '"'), t += ">" + et.avatar(r), S.sourceName[s.source] && (t += et.serviceIcon(s.source)), t += '</div><div class="ds-comment-body"><div class="ds-comment-header">', r.url ? (t += '<a class="ds-user-name ds-highlight" data-qqt-account="' + (r.qqt_account || "") + '" href="' + u(r.url) + '" ', r.user_id && (t += " onclick=\"this.href='" + a() + "/user-url/?user_id=" + r.user_id + "';\""), t += ' rel="nofollow" target="_blank"', r.user_id && (t += ' data-user-id="' + r.user_id + '"'), t += ">" + u(r.name) + ua(e.agent) + "</a>") : (t += '<span class="ds-user-name"', r.user_id && (t += ' data-user-id="' + r.user_id + '"'), t += ' data-qqt-account="' + (r.qqt_account || "") + '">' + u(r.name) + "</span>"), t += "</div>", 1 == i.max_depth && i.show_context && s.parents.length) {
                 t += '<ol id="ds-ctx">';
                 var n = lt.getJSON(s.parents);
                 if (n) for (var o, d = -1, l = n.length - 1; l > d;) o = n[d += 1], 1 == d && s.parents.length > 2 && (t += '<li class="ds-ctx-entry"><a href="javascript:void(0);" class="ds-expand">还有' + (s.parents.length - 2) + "条评论</a></li>"), t += et.ctxPost({
